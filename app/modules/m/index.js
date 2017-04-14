473df949a0691cdf0044227c566f507df42f51b8
Danne Leung <lshefan@163.com>
2017-04-14 11:15:05 +0800

    some changed

--- app/modules/m/index.js
+++ app/modules/m/index.js
@@ -4,10 +4,7 @@ var express = require('express');
 
 var ctrl = require('./index.controller');
 var router = express.Router();
-router.use((req, res, next) => {
-  res.locals.theme = "/themes/mzui";
-  return next();
-});
+
 router
   .get(['/'], ctrl.index)
   .get(['/home'], ctrl.home)
