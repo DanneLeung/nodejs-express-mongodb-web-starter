033b1499f590f5f4daa25e1098c1a052e8f041e1
DanneLeung <lshefan@163.com>
2017-04-18 00:52:33 +0800

    image upload

--- public/themes/dist/js/weixin.js
+++ public/themes/dist/js/weixin.js
@@ -313,7 +313,7 @@ var shortUrl = contextRoot + '/api/getShortUrl';
    */
   function chooseImage(callback) {
     wx.chooseImage({
-      count: 1, // 默认9
+      // count: 1, // 默认9
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
