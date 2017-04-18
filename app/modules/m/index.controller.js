777a33d317111e147316c62f93af41b1c920d85e
Danne Leung <lshefan@163.com>
2017-04-18 18:19:23 +0800

    wechat image upload

--- app/modules/m/index.controller.js
+++ app/modules/m/index.controller.js
@@ -54,6 +54,8 @@ exports.newSave = function (req, res) {
   var node = req.body.node;
   var openid = req.body.openid;
   var serviceIds = req.body.serviceIds || [];
+  console.log(" ************* topic body : ", req.body);
+
   if(!openid) {
     console.error(" openid");
     req.body.openid = openid = "oxVEQuN3xDA1r8aBD_hh-xMQeir4";
@@ -66,7 +68,7 @@ exports.newSave = function (req, res) {
   //读取微信公众号配置
   Wechat.findByAppid(appid, (err, wechat) => {
     if(err) res.status(200).json({ error: 1, msg: '公众号配置信息错误，图片无法上传' });
-    var mutil = mediaUtil(wechat.appid, wechat.appsecret);
+    var mutil = mediaUtil(appid, wechat.appsecret);
     async.map(serviceIds, (serviceId, callback) => {
       mutil.getMedia(serviceId, (err, wm) => {
         return callback(err, wm ? wm.path : '');
