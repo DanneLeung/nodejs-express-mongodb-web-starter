ea5d4a7f50764c6228597fc38efb740ba0150ced
DanneLeung <lshefan@163.com>
2017-04-18 22:38:08 +0800

    wechat image upload

--- app/modules/m/index.controller.js
+++ app/modules/m/index.controller.js
@@ -73,7 +73,7 @@ exports.newSave = function (req, res) {
   //读取微信公众号配置
   Wechat.findByAppid(appid, (err, wechat) => {
     if(err) res.status(200).json({ error: 1, msg: '公众号配置信息错误，图片无法上传' });
-    var mutil = mediaUtil(appid, wechat.appsecret);
+    var mutil = new mediaUtil(appid, wechat.appsecret);
     async.map(serviceIds, (serviceId, callback) => {
       mutil.getMedia(serviceId, (err, wm) => {
         return callback(err, wm ? wm.path : '');
