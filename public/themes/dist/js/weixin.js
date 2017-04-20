58f28c08702d9df27a3de2c5532aed38a1af5316
Danne Leung <lshefan@163.com>
2017-04-20 13:59:02 +0800

    fpm

--- public/themes/dist/js/weixin.js
+++ public/themes/dist/js/weixin.js
@@ -344,8 +344,7 @@ var shortUrl = contextRoot + '/api/getShortUrl';
       localId: localId, // 需要上传的图片的本地ID，由chooseImage接口获得
       isShowProgressTips: 1, // 默认为1，显示进度提示
       success: function (res) {
-        var serverId = res.serverId; // 返回图片的服务器端ID
-        callback(serverId);
+        callback(res.serverId);
       }
     });
   }
