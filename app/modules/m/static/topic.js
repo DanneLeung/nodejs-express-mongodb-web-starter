3ef727540fd60c12cee26ebab2ae0c6b5ae2a3c0
Danne Leung <lshefan@163.com>
2017-04-24 17:48:23 +0800

    fpm

--- app/modules/m/static/topic.js
+++ app/modules/m/static/topic.js
@@ -16,11 +16,11 @@ $(document).ready(function () {
     }, 'html');
   });
 
-  $(".addComment").on("click", function(e){
+  $(".addComment").on("click", function (e) {
     $("#newComment").show();
     $("#newComment").find("textarea").focus();
   });
-  $(".cancelComment").on("click", function(e){
+  $(".cancelComment").on("click", function (e) {
     $("#newComment").hide();
     $("#newComment").find("textarea").val('');
   });
@@ -52,19 +52,29 @@ $(document).ready(function () {
       //- alert(" upload image "+ localId);
       if(serverId) {
         serverIds.push(serverId);
-        wx.getLocalImgData({
-          localId: localId, // 图片的localID
-          success: function (res) {
-            var localData = res.localData; // localData是图片的base64数据,可以用img标签显示
-            var img = "<img src='" + localData + "'>"
-            var html = "<div class='cell-4'><div class='avatar.avatar-xl'>" + img + "</div></div>";
-            $("#images").append(html);
-            if(localIds.length > 0) {
-              syncUpload(localIds);
+        if(browser.versions.ios) {
+          wx.getLocalImgData({
+            localId: localId, // 图片的localID
+            success: function (res) {
+              var localData = res.localData; // localData是图片的base64数据,可以用img标签显示
+              var img = "<img src='" + localData + "'>"
+              var html = "<div class='cell-4'><div class='avatar.avatar-xl'>" + img + "</div></div>";
+              $("#images").append(html);
+              if(localIds.length > 0) {
+                syncUpload(localIds);
+              }
             }
+          });
+        } else {
+          // 非iOS情况，直接显示localid
+          var img = "<img src='" + localId + "'>";
+          var html = "<div class='cell-4'><div class='avatar.avatar-xl'>" + img + "</div></div>";
+          $("#images").append(html);
+          if(localIds.length > 0) {
+            syncUpload(localIds);
           }
-        });
+        }
       }
     });
-  };
+  }
 });
