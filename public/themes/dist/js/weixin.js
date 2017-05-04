a4a98fd04aef550d7bd3f0ae3ad6148d2fcd3d8f
DanneLeung <lshefan@163.com>
2017-05-05 00:52:39 +0800

    some changed

--- public/themes/dist/js/weixin.js
+++ public/themes/dist/js/weixin.js
@@ -77,9 +77,8 @@ var shortUrl = contextRoot + '/api/getShortUrl';
    * @param redirectUri
    */
   function oauth(appid, scope, redirectUri, state) {
-    var redirectURL = encodeURIComponent('http://9cubic.cn/wxroute/' + redirectUri.replace('https://', '').replace('http://', ''));
-    // var redirectURL = encodeURIComponent(redirectUri);
-    //var redirectURL = encodeURIComponent(redirectUri);
+    // var redirectURL = encodeURIComponent('http://9cubic.cn/wxroute/' + redirectUri.replace('https://', '').replace('http://', ''));
+    var redirectURL = encodeURIComponent(redirectUri);
     var oauthURL = weixinOauthURL;
     oauthURL += '?appid=' + appid;
     oauthURL += '&redirect_uri=' + (redirectURL);
@@ -161,9 +160,9 @@ var shortUrl = contextRoot + '/api/getShortUrl';
           window.location.href = removeQueryString(redirectUri, ['code', 'state']);
         });
       }
-    // } else if(user && openid && openid != '' && openid != 'null') { //console.log('获取到本地用户信息');
-    //   console.log('获取到本地用户信息'); //console.log('获取到本地用户信息');
-    //   return callback(user ? JSON.parse(user) : {});
+      // } else if(user && openid && openid != '' && openid != 'null') { //console.log('获取到本地用户信息');
+      //   console.log('获取到本地用户信息'); //console.log('获取到本地用户信息');
+      //   return callback(user ? JSON.parse(user) : {});
     } else if(openid && openid != '' && openid != 'null') {
       return getUserAndSave(appid, openid, callback);
     } else if(unionid) {
@@ -210,7 +209,7 @@ var shortUrl = contextRoot + '/api/getShortUrl';
   }
 
   function userInfoLocalStorage(appid, user) {
-    if(!user){
+    if(!user) {
       localStorage.clear();
       return user;
     }
@@ -317,7 +316,7 @@ var shortUrl = contextRoot + '/api/getShortUrl';
    */
   function chooseImage(count, callback) {
     if(!count) count = 1;
-    if(count > 9) count=9;
+    if(count > 9) count = 9;
     wx.chooseImage({
       count: count, // 默认9
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
@@ -389,8 +388,8 @@ var shortUrl = contextRoot + '/api/getShortUrl';
     });
   }
 
-  function clear(){
-    if(localStorage){
+  function clear() {
+    if(localStorage) {
       localStorage.clear();
     }
   }
