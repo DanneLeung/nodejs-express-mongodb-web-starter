97f5259c4959108f3210ed23643262e870a50be1
Danne Leung <lshefan@163.com>
2017-05-04 17:17:23 +0800

    fpm

--- helper/views-helper.js
+++ helper/views-helper.js
@@ -66,7 +66,7 @@ function helpers(name) {
     };
     res._redirect = res.redirect;
     res.redirect = function (uri) {
-      var url = _.startsWith(uri, 'http') ? uri : (req.session.contextRoot || "" + uri);
+      var url = _.startsWith(uri, 'http') ? uri : ((req.session.contextRoot || "") + uri);
       console.log(" >>>>>>>>>>>>>>>>>.. redirect to ", url);
       res._redirect(url);
     };
